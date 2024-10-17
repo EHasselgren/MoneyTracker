cca851fccda561c6da3f44df50b53761a89fb357
elias hasselgren <elias.hasselgren@icloud.com>
2024-10-17 16:47:14 +0200

    Final edit, minor structure update

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ namespace MoneyTracker
     public static class Program
     {
         static readonly ItemService _itemService = new();
-        static readonly DisplayService _displayService = new(_itemService);
         static readonly InputService _inputService = new();
+        static readonly DisplayService _displayService = new(_itemService);
         static readonly MenuService _menuService = new(_itemService, _displayService, _inputService);
 
         public static void Main()
